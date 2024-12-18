7376764b0a20d874c2ee8b81e204eb44f4a0df22
Fernando Acevedo Colman <fernando@nuvaweb.com>
2024-12-18 20:14:08 +0100

    adaptación a cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  // Integraciones que estás utilizando
+  integrations: [
+    tailwind(), 
+    cloudflare() // Integración de Cloudflare
+  ],
+  // Configuración del adaptador de salida
+  output: 'server', // Específico para Cloudflare Workers
+  adapter: cloudflare(), // Configura el adaptador de Cloudflare
 });
